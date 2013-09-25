83f99144ef2206b1270faac3007d9540d3b9cfd1
William Bagayoko <wbagayoko@madison.com>
2013-09-25 15:39:13 -0500

    Remember Me, Logoff, Styling

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ PHP Password Protected Template
 
 Very simple PHP app to password protect web applications.
 
-Change the username, password and next (the file to load once your approved) in the config.ini and your set. 
+Change the username, password, title, remember hash, and next (the file to load once your approved) in the config.ini and your set. 
 
 Happy Coding! 
 
--- public/index.php
+++ public/index.php
@@ -1,30 +1,64 @@
 <?php
 	session_start();
 
+	$error = null;
 	$ini_array = parse_ini_file("../config.ini");
+	$cookiehash = sha1($ini_array["remember"] . $_SERVER['REMOTE_ADDR'] . "wbyoko");
 
-	if ($_SESSION['authenticated'] == true) {
-		include($ini_array["next"]);
-	} else {
-		$error = null;
-		if (!empty($_POST)) {
-			$username = empty($_POST['username']) ? null : $_POST['username'];
-			$password = empty($_POST['password']) ? null : $_POST['password'];
-
-			if ($username == $ini_array["username"] && $password == $ini_array["password"]) {
-				$_SESSION['authenticated'] = true;
-				header('Location: index.php');
-				return;
-			} else {
-				$error = 'Incorrect username or password';
-			}
-		}
-		echo $error;
-		?>
-			<form method="POST" action="index.php">
-				Username <input type="text" name="username" />
-				Password <input type="password" name="password" />
-				<input type="submit" value="login" />
-			</form>
-		<?php
+	// logoff
+	if (!empty($_POST['logoff'])) {
+		$_POST = null;
+		$_SESSION['authenticated'] = false;
+		setcookie("remember", "", time() - 3600);
 	}
+
+	// try login
+	if (!empty($_POST)) {
+		$username = empty($_POST['username']) ? null : $_POST['username'];
+		$password = empty($_POST['password']) ? null : $_POST['password'];
+
+		if ($username === $ini_array["username"] && $password === $ini_array["password"]) {
+			$_SESSION['authenticated'] = true;
+			if ($_POST['remember'] === "on") {
+				setcookie("remember", $cookiehash, time()+3600*24*365);
+			}
+		} else {
+			$error = 'Incorrect username or password';
+		}
+	}
+
+	// display view
+	if ($_SESSION['authenticated'] == true || strcmp($cookiehash, $_COOKIE["remember"]) === 0) {
+		include($ini_array["next"]);
+	} else { ?>
+		<!doctype html>
+		<html lang="en">
+			<head>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			    <?php if (isset($ini_array["title"])) echo  "<title>" . $ini_array["title"] . "</title>"; ?>
+			</head>
+			<body>
+				<?php if (!empty($error)) echo  "<p>" . $error . "</p>"; ?>
+				<form method="post" style="margin: 50px auto; width: 33%;">
+					<fieldset>
+			    		<?php if (isset($ini_array["title"])) echo  "<legend>" . $ini_array["title"] . "</legend>"; ?>
+
+						<label for="username">Username</label>
+						<input id="username" name="username" type="text" placeholder="Username">
+						<br />
+
+						<label for="password">Password</label>
+						<input id="password" name="password" type="password" placeholder="Password">
+						<br />
+
+						<label for="remember">Remember me</label>
+						<input id="remember" name="remember" type="checkbox">
+						<br />
+						
+						<button type="submit" value="login">Sign in</button>
+					</fieldset>
+				</form>
+			</body>
+		</html>
+	<?php }
